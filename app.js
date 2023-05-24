2f58e04cf4ab559c9f5eb9ce38f20961852e76d6
Samuel Bacay <revolutionnaire@users.noreply.github.com>
2023-05-24 10:47:29 +0800

    Seventh attempt to fix internal server error

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const viewportDataCollection = db.collection('viewportData');
 
 // Capture viewport data
 app.get('/', (req, res) => {
-  res.render('views/home');
+  res.render('views/home.ejs');
 });
 
 app.post('/capture', (req, res) => {
@@ -59,7 +59,7 @@ app.get('/statistics', (req, res) => {
     .then(querySnapshot => {
       const viewportData = querySnapshot.docs.map(doc => doc.data());
       const averageViewport = calculateAverageViewport(viewportData);
-      res.render('views/statistics', { averageViewport });
+      res.render('views/statistics.ejs', { averageViewport });
     })
     .catch(error => {
       console.error('Error retrieving viewport data:', error);
